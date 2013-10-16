d10882e4c3bc83e7811b9cd7e705629094499d75
r02548 <bruger@fullsail.edu>
2013-10-16 19:20:16 -0400

    Setup input prompts.

--- GOLDAPP/js/main.js
+++ GOLDAPP/js/main.js
@@ -4,11 +4,20 @@ $('#home').on('pageinit', function(){
                                
 });	
 		
-$('#addItem').on('pageinit', function(){
+$('#additem').on('pageinit', function(){
 
 		var myForm = $('#addItemForm');
 		    myForm.validate({
 			invalidHandler: function(form, validator) {
+                            errorMsgs.click();
+                            var html = '';
+                            for (var key in validator.submitted) {
+                                var label = $('label[for^="' + key + '"]').not('[generated]');
+                                var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+                                var fieldName = legend.length ? legend.text(): label.text();
+                                html +='<li>' + fieldName +'</li>';
+                            };
+                            $("#errorMessages ul").html(html)
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
