244c2533c6a47b8c1c525d7f6129891252610680
r02548 <bruger@fullsail.edu>
2013-10-16 22:34:55 -0400

    Setup function to save data to local storage.
    Still not functioning correctly.

--- GOLDAPP/js/main.js
+++ GOLDAPP/js/main.js
@@ -39,11 +39,54 @@ var getData = function(){
 
 };
 
-var storeData = function(data){
-	
-}; 
+function getSelectedCheckedBoxes() {
+        if ($('medication1').checked) {
+            fleaValue = $('medication1').value;
+        } else {
+            fleaValue = "No"
+        };
+        if ($('medication2').checked){
+            heartwormValue = $('medication2').value;
+        } else {
+            heartwormValue = "No"
+        };
+        if ($('medication3').checked) {
+            otherValue = $('medication3').value;
+        } else{
+            otherValue = "No"
+        };
+    }
 
-var	deleteItem = function (){
+var storeData = function(data){
+    
+    if (!key) {
+        var generateId = Math.floor(Math.random()*100000001);
+        }else{
+            //Set the id to the existing key we're editing, so it will save over the data.
+            generateId = key;
+        }
+        
+        //Gather up all our form field values and store in an object.
+        //Object properties contain array with the form label and input value.
+        getSelectedCheckedBoxes(); 
+        var itemList                = {};
+            itemList.name           = ["Pet Name:", $('name').value];
+            itemList.age            = ["Pet Age:", $('age').value];
+            itemList.type           = ["Pet Type:", $('type').value];
+            itemList.medication1    = ["Flea:", fleaValue];
+            itemList.medication2    = ["Heartworm:", heartwormValue];
+            itemList.medication3    = ["Other:", otherValue];
+            itemList.reminderDate   = ["Date:", $('reminderDate').value];
+            itemList.scale          = ["Range:", $('scale').value];
+            itemList.notes          = ["Note:", $('notes').value];
+            
+            //Save data into Local Storage
+            localStorage.setItem(generateId, JSON.stringify(itemList));
+            alert("Reminder has been added!");
+    };
+	
+
+var deleteItem = function (){
 			
 };
 					
@@ -51,4 +94,10 @@ var clearLocal = function(){
 
 };
 
+//Variable Defaults
+var fleaValue;
+var heartwormValue;
+var otherValue;
+var createButton = $('submitButton');
+createButton.addEventListener("click", storeData);
 
